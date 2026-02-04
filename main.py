7cb972f7ec00dbe5ee0de853edeefcd7a75927ec
uiv14162 <eduard.ghiuzan@vitesco.com>
2026-02-04 13:48:49 +0200

    Project Init

---
+++ main.py
@@ -0,0 +1,9 @@
+from card import Card
+from deck import Deck
+from hand import Hand
+from player import Player
+def main():
+    print("Blackjack game setup complete.")
+
+if __name__ == "__main__":
+    main()
