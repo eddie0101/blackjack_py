0dad304aadf23d96c51be5d0e49c392c5cc73981
uiv14162 <eduard.ghiuzan@vitesco.com>
2026-02-04 15:01:48 +0200

    Added basic functionality fo deck class

--- deck.py
+++ deck.py
@@ -1,2 +1,20 @@
+from card import Card
+
 class Deck:
-    pass
+    def __init__(self):
+        self.cards = []
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(rank, suit))
+    
+    def shuffle(self):
+        import random
+        random.shuffle(self.cards)
+    
+    def deal_card(self):
+        return self.cards.pop() if self.cards else None
+    
+    def __str__(self):
+        return ', '.join(str(card) for card in self.cards)
