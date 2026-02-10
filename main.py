52736a1e99bcc7088d0f9312010e829791adb59c
uiv14162 <eduard.ghiuzan@vitesco.com>
2026-02-10 14:47:07 +0200

    Set up initial game state by shuffling the deck and dealing cards to the player

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from deck import Deck
 from hand import Hand
 from player import Player
 def main():
-    print("Blackjack game setup complete.")
+    deck = Deck()
+    deck.shuffle()
+    player = Player("Eddie")
+    for _ in range(2):
+        dealt_card = deck.deal_card()
+        player.recieve_card(dealt_card)
+    print(player)
 
 if __name__ == "__main__":
     main()
+
